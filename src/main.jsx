a86fca0dcb6d49fe72a4319c066efdfab2d55983
rodrigoniskier <niskier.rodrigo@gmail.com>
2025-11-14 11:29:08 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// ... (depois de todos os 'imports' e de 'root.render(...)')
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('Service Worker registrado com sucesso:', registration);
+      })
+      .catch((error) => {
+        console.log('Falha ao registrar o Service Worker:', error);
+      });
+  });
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
